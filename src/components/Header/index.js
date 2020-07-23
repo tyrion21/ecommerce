6dbf9493debf4d84f076b15de2001cdadb75c7a1
unknown <operacionesncc@OP06-CC-NCC.interclinica.local>
2020-07-23 17:48:48 -0400

    version 3

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './styles.scss';
 import Logo from './../../assets/berserk_logo.png'
+import { Link } from 'react-router-dom';
 
 const Header = props => {
     return(
         <header className="header">
             <div className="wrap">
                 <div className="logo">
-                    <img src={Logo} alt="Berserk Store Logo"/>
+                    <Link to="/">
+                        <img src={Logo} alt="Berserk Store Logo"/>
+                    </Link>                    
+                </div>
+
+                <div className="callToActions">
+                    <ul>
+                        <li>
+                        <Link to="/registration">
+                            Register
+                        </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </header>
--- src/layouts/HomepageLayout.js
+++ src/layouts/HomepageLayout.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const HomepageLayout = props => {
     return (
         <div className="fullHeight">
         <Header />
-        <div className="main">
             {props.children}
+        <Footer />           
         </div>
-    </div>
     );
 };
 
